8fde2149e5f8ecd6498a23a3898addb7c4daab30
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-02 16:14:33 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const String = window.String;
 const Reflect = window.Reflect;
-const { get, set ,ownkeys} = Reflect;
+const { get, set ,ownKeys} = Reflect;
 const valuestring = "value";
 function isobject(a: any): a is object {
   return typeof a === "object" && a !== null;
@@ -173,7 +173,7 @@ const isinputtextortextareaflag = isinputtextortextarea(ele);
       return true;
     },
     has(target, key) {
-return ownkeys(outputattrs).includes(key)
+return ownKeys(outputattrs).includes(key)
 /*
 const isinputtextortextareaflag = isinputtextortextarea(ele);
   
