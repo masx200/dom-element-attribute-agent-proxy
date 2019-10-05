a863490bb520a427ec5ac697e316da7611ef4957
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-05 11:49:40 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+const camelizeRE = /-(\w)/g
+export const camelize = (str: string): string => {
+  return str.replace(camelizeRE, (_, c) => (c ? c.toUpperCase() : ''))
+}
+
+const hyphenateRE = /\B([A-Z])/g
+export const hyphenate = (str: string): string => {
+  return str.replace(hyphenateRE, '-$1').toLowerCase()
+}
 const String = window.String;
 const Reflect = window.Reflect;
 const { get, set ,ownKeys} = Reflect;
