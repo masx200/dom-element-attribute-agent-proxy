17ffb6bada78d6f25984a1b7a609bcb678e102c2
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-22 08:35:16 +0800

    Update mustUseDomProp.js

--- src/mustUseDomProp.js
+++ src/mustUseDomProp.js
@@ -1,7 +1,6 @@
 const acceptValue = ['input', 'textarea', 'option', 'select']
 
-export default (tag, attr) => {
-const type=tag
+export default (tag, type, attr) => {
   return (
     (attr === 'value' && acceptValue.includes(tag) && type !== 'button') ||
     (attr === 'selected' && tag === 'option') ||
