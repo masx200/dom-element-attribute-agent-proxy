2676d6b112249d82e1ae2455e4ab842d5ab77f56
masx200 <34191203+masx200@users.noreply.github.com>
2019-10-22 08:37:42 +0800

    Update and rename mustUseDomProp.js to mustUseDomProp.ts

--- src/mustUseDomProp.ts
+++ src/mustUseDomProp.ts
@@ -1,8 +1,8 @@
 const acceptValue = ['input', 'textarea', 'option', 'select']
 
-export default (tag, type, attr) => {
+export default (tag, attrtype, attr) => {
   return (
-    (attr === 'value' && acceptValue.includes(tag) && type !== 'button') ||
+    (attr === 'value' && acceptValue.includes(tag) && attrtype !== 'button') ||
     (attr === 'selected' && tag === 'option') ||
     (attr === 'checked' && tag === 'input') ||
     (attr === 'muted' && tag === 'video')
